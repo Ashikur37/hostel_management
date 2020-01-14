80477d8c0c460639caff120aded54f7a1ea2f7c6
ashikur3700 <asikur.rahman@eongroup.net.bd>
2020-01-15 00:52:46 +0600

    updated

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -7,35 +7,63 @@
         padding-top: 10px;
     }
 </style>
-<div class="ju">
-    <center>
-        <h3>Notices</h3>
-    </center>
+<div class="jumbotron">
+    
     <div class="row">
             <div class="col-md-1"></div>
             <div class="col-md-10">
-            <table class="table table-stripped table-bordered">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Title</th>
-                        <th>Details</th>
-                        <th>Published At</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @php ($i = 1)
-                @foreach ($notices as $notice)
-            <tr>
-                <td>{{$i}}</td>
-                <td>{{$notice->title}}</td>
-                <td>{{$notice->body}}</td>
-                <td>{{$notice->created_at}}</td>
-                @php ($i++)
-            </tr>
-                @endforeach
-                </tbody>
-            </table>
+            
+
+<div class="card card-prirary cardutline direct-chat direct-chat-primary">
+        <div class="card-header">
+          <h3 class="card-title">Notice Board</h3>
+        </div>
+        <div class="card-body">
+                <table class="table table-stripped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>SL</th>
+                                
+                                <th>Title</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Student can be closed.</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>All students can attend in competitions.</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Student can use sports elements.</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Students  can’t  be taken  any guest without permission.</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>Students can’t  be enter hostel  at  7 pm.(for girls).</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>Students can’t  be enter hostel  at  7 pm.(for boys).</td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>All students must  be under  in disciplined.</td>
+                            </tr>
+                               
+                                
+                        </tbody>
+                    </table>
+        </div>
+</div>
+
+
             </div>
     </div>
     
@@ -44,7 +72,9 @@
 
     
 </div>
-<script>
-
+<<script>
+window.setTimeout(function(){
+        document.getElementById("footer").style.position="relative";
+    },100)
 </script>
 @endsection
--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -11,51 +11,77 @@
     <center>
         <h3>Rules & Regulation</h3>
     </center>
-    <ol >
+    <h3 style="color:green">Bring your kind attention</h3>
+
+<ol type="a" >
         <li>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi odit tempore maxime alias corporis deleniti neque, in obcaecati pariatur quasi temporibus ut. Labore aliquid laboriosam eaque facilis unde maiores molestias!
+           To get allotment a seat in the hostel application should be made to the hostel super through the Head of the Department of each Department.
         </li>
         <li>
-            lorem
+           Under taking that they would abide by the following rules & take the seats from Hostel Super if get allotment.
+        </li>
+		</ol>
+		<br>
+		
+		<h3 style="color:green">Rules & Regulations</h3>
+		
+		<ol type="1">
+        <li>
+            Rent for seat for "Boys Hostel-1"=1400 Tk ,"Boys Hostel-2"=1650 Tk And "Girls Hostel"=5000 Tk(Seat rent=2000 Tk & Meal Charge=3000 Tk)
+			should be deposit in the Bank by 10th of every month and receipt must be handover to the Hostel super.
+			</li>
+        <li>
+            All the member staying in Double room unite should be paid Electricity bill to Hostel Super.
         </li>
         <li>
-            lorem
+            If anybody, for any reason wants to leave the hostel,they must pay the dues and let the matter to the Hostel Super 1 month prior and must
+			collect no objection certificate.
         </li>
         <li>
-            lorem
+            If any damage of the room or articles supplied by The University Authority must be replaced or repaired by the student's own Account.
         </li>
         <li>
-            lorem
+            At any circumstances,no guests are allowed in the Hostel.Students are allowed only to meet the guest at guestroom.
         </li>
         <li>
-            lorem
+            Hostel Authority can change or cancel any allotment if required.
         </li>
         <li>
-            lorem
+            
         </li>
         <li>
-            lorem
+            Every students must return to Hostel by 8 pm. Prior permission must be taken,if anyone wants to stay outside for urgent need.
         </li>
         <li>
-            lorem
+            Hostel super must be learnt if any student feel sick,if required advice make be taken by a particular Doctor or communicate to gurdians.
         </li>
         <li>
-            lorem
+            Clinliness of the hostel must be done by students jointly..
+        </li>
+		
+		
+		
+	<h3 style="color:green">Bring your kind notice</h3>
+        <li>
+            Authority must be apprised of any problem or insecurity if they face.
         </li>
         <li>
-            lorem
+            At any cicumstances,seats or any articles can't be exchanged among each other.
         </li>
-        <li>
-            lorem
-        </li>
-        <li>
-            lorem
-        </li>
-        <li>
-            lorem
-        </li>
+<li>
+		 Authority must be apprised of any body violets rules.
+		   If any irrigularities noticed by the Hostel Super ,apprising this to university disciplinery comitte,
+		   he can take any legal action against the particular student or students.
+		   No objections from students regarding the matter are allowed.
+		
+		
+		</li>
+		</ol>
+		
+		
 
     </ol>
+
 </div>
 <script>
 window.setTimeout(function(){
